fbe8d04020d0b225648f1ac6c786520ef9db1a44
Rishija <rishijamangla@gmail.com>
2018-03-25 17:23:31 +0530

    Review

---
+++ leetcode/Q20_valid_parentheses.cpp
@@ -0,0 +1,68 @@
+/*
+ Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ 
+ The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+ */
+/*
+ Time, space : O(n)
+ */
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+bool isValid(string s) {
+    
+    int n = s.size();
+    if(!n)
+        return true;
+    if(n % 2)
+        return false;
+    
+    stack<char>stack;
+    for(int i = 0; i < n; ++i) {
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+            stack.push(s[i]);
+        else {
+            if(stack.size() &&
+               ((s[i] == ')' && stack.top() == '(') ||
+                (s[i] == '}' && stack.top() == '{') ||
+                (s[i] == ']' && stack.top() == '[') ) )
+                
+                stack.pop();
+            else
+                return false;
+        }
+    }
+    return stack.empty();
+}
+
+int main() {
+    
+    string s = "()";
+    cout << isValid(s) << endl;
+    
+    s = "()[]{}";
+    cout << isValid(s) << endl;
+    
+    s = "({[]})";
+    cout << isValid(s) << endl;
+    
+    s = "[{()()}({{}})]";
+    cout << isValid(s) << endl;
+    
+    s = "(]";
+    cout << isValid(s) << endl;
+    
+    s = "([)]";
+    cout << isValid(s) << endl;
+    
+    s = "}]";
+    cout << isValid(s) << endl;
+    
+    s = "((";
+    cout << isValid(s) << endl;
+    
+    return 0;
+}
+
---
+++ leetcode/Q22_generate_parantheses.cpp
@@ -0,0 +1,84 @@
+/*
+ Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+void helper(vector<string> &ans, string current, int open, int close) {
+    
+    if(!open && !close) {
+        ans.push_back(current);
+        return;
+    }
+    
+    if(open == close){
+        helper(ans, current + "(", open - 1, close);
+        return;
+    }
+    
+    if(close)
+        helper(ans, current + ")" , open, close - 1);
+    if(open)
+        helper(ans, current + "(", open - 1, close);
+}
+
+vector<string> generateParenthesis(int n) {
+    
+    if(!n)
+        return {};
+    
+    if(n == 1)
+        return {"()"};
+    
+    vector<string> ans;
+    helper(ans, "", n, n);
+    
+    return ans;
+}
+
+
+void find_print(int n) {
+    vector<string> temp = generateParenthesis(n);
+    cout << n << endl;
+    for(int i = 0; i < temp.size(); ++i)
+        cout << temp[i] << endl;
+    cout << endl;
+}
+
+int main() {
+    
+    for(int i = 0; i < 5; ++i)
+        find_print(i);
+    
+    return 0;
+}
+
+/* Bad approach */
+vector<string> generateParenthesis1(int n) {
+    
+    if(!n)
+        return {};
+    
+    if(n == 1)
+        return {"()"};
+    
+    set<string> thisSet;
+    vector<string> prev = generateParenthesis1(n - 1);
+    
+    for(string elem : prev) {
+        for(int j = 0; j < elem.size(); ++j) {
+            string temp = elem.substr(0, j) + "()" + elem.substr(j);
+            if(thisSet.find(temp) == thisSet.end())
+                thisSet.insert(temp);
+        }
+    }
+    vector<string> ans;
+    for(string elem : thisSet)
+        ans.push_back(elem);
+    
+    return ans;
+}
