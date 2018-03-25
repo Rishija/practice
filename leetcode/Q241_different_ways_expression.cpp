ee5e0a6c4dd8d1b1d252072f50efeb4ad2bc15d3
pramnegi <negi.1984@gmail.com>
2018-03-25 07:10:36 -0700

    Update Q241_different_ways_expression.cpp

--- leetcode/Q241_different_ways_expression.cpp
+++ leetcode/Q241_different_ways_expression.cpp
@@ -8,7 +8,6 @@
 using namespace std;
 
 int evaluate(int a, int b, char op) {
-    
     if(op == '+')
         return a + b;
     if(op == '-')
@@ -19,9 +18,7 @@ int evaluate(int a, int b, char op) {
     return 0;   // Xcode compulsion
 }
 
-
 vector<int> helper(string s, int start, int end, vector<vector<vector<int> > > &dp) {
-    
     if(dp[start][end].size() == 0) {
         int i = start;
         string temp = "";
@@ -29,21 +26,21 @@ vector<int> helper(string s, int start, int end, vector<vector<vector<int> > > &
             temp += s[i];
             ++ i;
         }
+
         if(i == end + 1)
             dp[start][end].push_back(stoi(temp));
-        
         else {
-            
             vector<int> leftPossile, rightPossible;
-            
             for(int i = start; i <= end; ++i) {
                 if(s[i] == '*' || s[i] == '+' || s[i] == '-') {
                     leftPossile = helper(s, start, i - 1, dp);
                     rightPossible = helper(s, i + 1, end, dp);
                     
-                    for(int left : leftPossile)
-                        for(int right : rightPossible)
+                    for(int left : leftPossile) {
+                        for(int right : rightPossible) {
                             dp[start][end].push_back(evaluate(left, right, s[i]));
+                        }
+                    }
                 }
             }
         }
@@ -51,17 +48,13 @@ vector<int> helper(string s, int start, int end, vector<vector<vector<int> > > &
     return dp[start][end];
 }
 
-
 vector<int> diffWaysToCompute(string input) {
-    
     int n = input.size();
-    
     vector<vector<int> > temp1(n, vector<int> (0));
     vector<vector<vector<int> > > dp(n, temp1);
-    
+ 
     return helper(input, 0, n - 1, dp);
 }
-
 
 void find_print(string &s) {
     vector<int> temp = diffWaysToCompute(s);
@@ -72,12 +65,11 @@ void find_print(string &s) {
 
 
 int main() {
-    
     string s = "2-1-1";
     find_print(s);
     
     s = "2*3-4*5";
     find_print(s);
-    
+
     return 0;
 }
